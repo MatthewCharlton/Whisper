012c9a18ca61ee3ef6a6a2d1f95878b654f149aa
Charlton <>
2018-09-01 22:38:32 +1000

    add reactive functionality

--- src/clientSide/index.js
+++ src/clientSide/index.js
@@ -1,19 +1,31 @@
-class WhisperClientSide {
-  constructor() {
-    this.data = { blah: "alksbfkoasb" };
-  }
-  getState() {
-    new CustomEvent("getState", {
+const whisper = {
+  self: this,
+  GET_STATE: "getState",
+  subs: [],
+  data: {},
+  create(event, options) {
+    return new CustomEvent(event, options);
+  },
+  subscribe(element, event, eventName, fn) {
+    this.subs.push({
+      element: element,
+      listener: element.addEventListener(eventName, fn),
+      event: event
+    });
+  },
+  getState(element, fn) {
+    const event = this.create(this.GET_STATE, {
       bubbles: true,
       detail: this.data
     });
-  }
+    this.subscribe(element, event, this.GET_STATE, fn);
+  },
   setState(data) {
     this.data = Object.assign(this.data, data, {});
+    this.subs.forEach(function(item) {
+      console.log(item.element);
+      console.log(item.eventName);
+      item.element.dispatchEvent(item.event);
+    });
   }
-  connect(element, event) {
-    element.dispatchEvent(event);
-  }
-}
-
-const whisper = new WhisperClientSide();
+};
