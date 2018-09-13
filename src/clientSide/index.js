3deaf8e2439d077f69fe7be16337772afd5fe908
Matthew Charlton <matthew.charlton@qantas.com.au>
2018-09-13 12:44:11 +1000

    add polyfill, add react-demo

--- src/clientSide/index.js
+++ src/clientSide/index.js
@@ -1,5 +1,21 @@
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
 var Whisper = {
-  GET_STATE_EVENT_NAME: 'getState',
+  EVENT_NAME: 'getState',
   subs: [],
   detail: {},
   create(event, options) {
@@ -14,15 +30,15 @@ var Whisper = {
       event: event
     });
   },
-  getState(element, fn) {
-    var event = this.create(this.GET_STATE_EVENT_NAME, {
+  get(element, fn) {
+    var event = this.create(this.EVENT_NAME, {
       cancelable: true,
       bubbles: true,
       detail: this.detail
     });
-    this.subscribe(element, event, this.GET_STATE_EVENT_NAME, fn);
+    this.subscribe(element, event, this.EVENT_NAME, fn);
   },
-  setState(detail) {
+  set(detail) {
     this.detail = Object.assign(this.detail, detail, {});
     this.subs.forEach(function(item) {
       item.element.dispatchEvent(item.event);
